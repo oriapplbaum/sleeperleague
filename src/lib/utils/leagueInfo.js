db6dc6234c0b24c44926fc855c96ad6935e937f7
oriapplbaum <33549300+oriapplbaum@users.noreply.github.com>
2023-08-10 18:37:01 -0400

    Update leagueInfo.js
    Added dues for 2023

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "990743500603879424"; // your league ID
 export const leagueName = "Sharon Football"; // your league name
-export const dues = 100; // (optional) used in template constitution page
+export const dues = 20; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
