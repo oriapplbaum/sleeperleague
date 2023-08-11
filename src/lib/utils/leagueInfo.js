e7a9d025f5fe1e5fe1a230d9832eb189911a1c1a
oriapplbaum <33549300+oriapplbaum@users.noreply.github.com>
2023-08-11 13:01:13 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -120,7 +120,7 @@ export const managers = [
       "managerID": "468627309939650560",
       "location": "Omaha", // (optional)
       "bio": "My name is Ori or the back to back champ. 2 time league winner btw. I have too much free time and create stuff like this website. I will bother you about trades and spend too much time on the waiver wire. ",
-      "photo": "/managers/Orisleeper.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/ori.jpg", // square ratio recommended (no larger than 500x500)
      // "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "mia", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
