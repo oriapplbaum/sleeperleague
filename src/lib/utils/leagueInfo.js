adeec2a5dd8595e93860b1028b5e7e012949236d
oriapplbaum <33549300+oriapplbaum@users.noreply.github.com>
2023-08-11 12:15:43 -0400

    Update leagueInfo.js
    Added jakes picture to manager page

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -166,7 +166,7 @@ export const managers = [
       "managerID": "468629743978147840",
       "location": "NYC?", // (optional)
       "bio": "My name is Jake. I won in 2021 but I choked last year.  I am a sleeper merchant as I sucked before the switch",
-      "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/Jakesleeper.jpg", // square ratio recommended (no larger than 500x500)
      // "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "ne", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
