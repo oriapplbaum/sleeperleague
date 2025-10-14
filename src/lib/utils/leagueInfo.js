d47a56790b303dff0613183e68b020fa3931803a
oriapplbaum <33549300+oriapplbaum@users.noreply.github.com>
2025-10-14 10:10:28 -0400

    Update leagueInfo.js
    Updated league to 2025

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1123470732928831488"; // your league ID
+export const leagueID = "1254143802050871296"; // your league ID
 export const leagueName = "Sharon Football"; // your league name
 export const dues = 20; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
